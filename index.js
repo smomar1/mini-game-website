574e6c9ef101c0141b7bc4dc11d13e6387e90fa6
yhan4 <yhan.4@hotmail.com>
2023-11-01 20:57:05 -0700

    Add index.js to homepage

---
+++ index.js
@@ -0,0 +1,7 @@
+number_guesser.addEventListener('click', function() {
+    window.location.href = 'numberGuesser/numberGuesser.html';
+  });
+
+hangman.addEventListener('click', function() {
+    window.location.href = 'hangmanIndex.html';
+});
